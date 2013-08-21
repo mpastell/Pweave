884f5064f3a8938a4ccb2537abb256d5d4803bf2
Aaron O'Leary <aaron.oleary@gmail.com>
2013-08-21 22:59:46 +0100

    notebook convert: integration test conversion

---
+++ tests/test_ipynb.py
@@ -0,0 +1,9 @@
+import pweave
+
+
+def test_nbformat():
+    """Test whether we can write an IPython Notebook.
+    """
+    infile = 'tests/simple.mdw'
+    # pandoc_args = None skips the call to pandoc
+    pweave.convert(file=infile, informat="noweb", outformat="ipynb")
