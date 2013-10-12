f58620449f19d423dd89bf761e018704598f7f54
Aaron O'Leary <aaron.oleary@gmail.com>
2013-10-12 21:30:56 +0100

    notebook convert: test against good reference

--- tests/test_ipynb.py
+++
@@ -1,9 +0,0 @@
-import pweave
-
-
-def test_nbformat():
-    """Test whether we can write an IPython Notebook.
-    """
-    infile = 'tests/simple.mdw'
-    # pandoc_args = None skips the call to pandoc
-    pweave.convert(file=infile, informat="noweb", outformat="notebook")
