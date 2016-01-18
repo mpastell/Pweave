04160cffdf3a19641496a599ced9583ec81cd61a
= <matti.pastell@helsinki.fi>
2016-01-18 22:42:01 +0200

    Octave test again

--- tests/test_xoctave.py
+++ tests/test_xoctave.py
@@ -2,7 +2,6 @@ import pweave
 
 def test_xoctave():
     """Test Octave code"""
-    return True
     pweave.weave("tests/octave/octave_test.mdw", doctype = "leanpub", shell = "octave")
     assertSameContent("tests/octave/octave_test.txt", "tests/octave/octave_test_ref.md")
 
