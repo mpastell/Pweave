3b9b5b9f3947da548e7ae51c323aaac4acbc9521
Matti Pastell <matti.pastell@helsinki.fi>
2016-08-12 21:33:30 +0300

    Fix output directory bug in tangle and add test

---
+++ tests/publish/publish_test_REF.py
@@ -0,0 +1,46 @@
+
+from pylab import *
+import scipy.signal as signal
+
+#Plot frequency and phase response
+def mfreqz(b,a=1):
+    w,h = signal.freqz(b,a)
+    h_dB = 20 * log10 (abs(h))
+    subplot(211)
+    plot(w/max(w),h_dB)
+    ylim(-150, 5)
+    ylabel('Magnitude (db)')
+    xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
+    title(r'Frequency response')
+    subplot(212)
+    h_Phase = unwrap(arctan2(imag(h),real(h)))
+    plot(w/max(w),h_Phase)
+    ylabel('Phase (radians)')
+    xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
+    title(r'Phase response')
+    subplots_adjust(hspace=0.5)
+
+#Plot step and impulse response
+def impz(b,a=1):
+    l = len(b)
+    impulse = repeat(0.,l); impulse[0] =1.
+    x = arange(0,l)
+    response = signal.lfilter(b,a,impulse)
+    subplot(211)
+    stem(x, response)
+    ylabel('Amplitude')
+    xlabel(r'n (samples)')
+    title(r'Impulse response')
+    subplot(212)
+    step = cumsum(response)
+    stem(x, step)
+    ylabel('Amplitude')
+    xlabel(r'n (samples)')
+    title(r'Step response')
+    subplots_adjust(hspace=0.5)
+
+print("I'm publishing a term chunk")
+
+a = 12
+b = 10
+print(a+b)
--- tests/test_readers.py
+++ tests/test_readers.py
@@ -5,7 +5,20 @@ def test_markdown():
     pweave.weave("tests/readers/markdown_reader.pmd", doctype = "pandoc", informat = "markdown")
     assertSameContent("tests/readers/markdown_reader.md", "tests/readers/markdown_reader_ref.md")
 
+def test_script():
+    """Test markdown reader"""
+    doc = pweave.Pweb("tests/publish/publish_test.txt", informat="script")
+    doc.tangle()
+    assertSameContent("tests/publish/publish_test.py",
+                      "tests/publish/publish_test_REF.py")
+
+
+
 def assertSameContent(REF, outfile):
     out = open(outfile)
     ref = open(REF)
     assert (out.read() == ref.read())
+
+if __name__ == '__main__':
+    test_markdown()
+    test_script()
