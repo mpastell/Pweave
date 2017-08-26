5738fc9f806f6c2e175a34373179068c786f0e6c
Matti Pastell <matti.pastell@helsinki.fi>
2017-08-26 21:55:52 +0300

    Release 0.30

--- pweave/__init__.py
+++ pweave/__init__.py
@@ -11,8 +11,7 @@ from .processors import *
 from .config import *
 
 
-__version__ = '0.30rc1'
-
+__version__ = '0.30'
 
 def weave(file, doctype=None, informat=None, kernel="python3", plot=True,
           docmode=False, cache=False,
