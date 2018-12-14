01eb7a8febb59c6ac295fdb271986a14e355a848
Forest Gregg <fgregg@uchicago.edu>
2018-12-13 23:13:03 -0600

    pin scipy version to avoid numpy future warning that breaks test
    The current version of numpy warns about indexing a numpy array
    with a sequence other than a tuple. This happens in a few places
    in scipy 1.1.*, this will be fixed in 1.2.

--- .conda_ci_install.sh
+++ .conda_ci_install.sh
@@ -3,5 +3,5 @@
 set -e -x
 
 conda install --yes nomkl
-conda install --yes python=$TRAVIS_PYTHON_VERSION scipy matplotlib markdown pygments coverage ipython nose nbformat jupyter_client nbconvert notebook
+conda install --yes python=$TRAVIS_PYTHON_VERSION "scipy==1.2rc2" matplotlib markdown pygments coverage ipython nose nbformat jupyter_client nbconvert notebook
 conda install --yes -c conda-forge pandoc
